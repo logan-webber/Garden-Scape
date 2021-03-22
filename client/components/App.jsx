decc21fecdb57031111022c6b22afdcc3c1056a0
Logan Webber <logan.webber@hotmail.com>
2021-03-22 16:09:33 +1300

    added add plant to app and made a link to the new page

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -5,6 +5,7 @@ import OnePlant from './OnePlant'
 import Home from './Home'
 import { HashRouter as Router, Route } from 'react-router-dom'
 import { fetchPlants } from '../actions/index'
+import AddPlant from './AddPlant'
 
 // to auto indent alt + shift + f
 
@@ -22,6 +23,7 @@ function App(props) {
 				<Route path='/' exact component={Home} />
 				<Route path='/listofplants' exact component={ListOfPlants} />
 				<Route path='/oneplant/:id' exact component={OnePlant} />
+				<Route path='/addplant' exact component={AddPlant} />
 			</Router>
 		</>
 	)
--- client/components/Home.jsx
+++ client/components/Home.jsx
@@ -7,6 +7,7 @@ function Home () {
             <h1>This is the Home page</h1>
             <h2>It will be the main page of the website and provide links to navigate around</h2>
             <Link to='/listofplants'>List of plants</Link>
+            <Link to='/AddPlant'>Add a plant</Link>
         </>
     )
 }
