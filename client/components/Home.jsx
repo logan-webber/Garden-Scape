a65746cc26addf8ebeb92af91eb4f8ae4120fdd6
Logan Webber <logan.webber@hotmail.com>
2021-03-25 23:37:31 +1300

    reformatted main page

--- client/components/Home.jsx
+++ client/components/Home.jsx
@@ -1,13 +1,13 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 
-function Home () {
-    return(
+function Home() {
+    return (
         <>
             <h1>This is the Home page</h1>
-            <h2>It will be the main page of the website and provide links to navigate around</h2>
+            <h2>Click below to view and add plants</h2>
             <Link to='/listofplants'>List of plants</Link>
-            <br/>
+            <br />
             <Link to='/AddPlant'>Add a plant</Link>
         </>
     )
